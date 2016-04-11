7d26a3c69e2fbb9fc8701c2756200bcb6e532e2d
Zleub <debray.arnaud@gmail.com>
2016-04-11 18:51:41 +0200

    more table, more function

--- notions.lua
+++ notions.lua
@@ -31,9 +31,36 @@ return {
 				"lua> table.insert(t, 42)",
 				"lua> t = table.pack('Hello', 42, 'World')",
 				"lua> =table.unpack(t)",
-				"lua> table.remove(t, 1)",
+				"lua> table.remove(t, 1)"
 			}
 		},
+		{
+			name = "more table", category = "",
+			description = "Learn that table is power",
+			examples = {
+[[
+PNJ = {
+	name = 'pnj'
+	life = 20,
+	toString = function (self) return self.name..', '..self.life end,
+}
+
+PNJ:toString()
+]],
+[[
+function Class(baseClass)
+	baseClass.__index = function (t, i)
+		return getmetatable(t)[i]
+	end
+	return function (arg)
+		return setmetatable(arg or {}, baseClass)
+	end
+end
+
+Foo = Class({name = 'foo'})
+=Foo().name
+]]
+			}
 		-- {
 		-- 	name = "local", category = "keyword",
 		-- 	description = "Learn what is a chunk",
@@ -82,6 +109,33 @@ return {
 				"lua> =(function (x, y) return x, y end)(4, 2)"
 			}
 		},
+		{
+			name = "more function", category = "keyword closure",
+			description = "Learn that function is power",
+			examples = {
+[[
+function iter(t)
+	local i = 0
+	return function ()
+		local e = t[i]
+		i = i + 1
+		return t[i]
+	end
+end
+
+for _ in iter({41, 42, 43}) do print(_) end
+]], [[
+function Function(arg)
+	print(arg.name)
+	return function (...)
+		return arg.fn(...)
+	end
+end
+
+f = Function{ name="sum", fn=function (x, y) return x + y end }
+]]
+			}
+		}
 	},
 
 	-- paradigms = {
